2f5685640e36c2f56c88af320632294757ede1bf
Andrew-UA <darwenua@gmail.com>
2024-02-26 15:38:37 +0200

    Upload files

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"blockchain/utils"
+	"fmt"
+)
+
+func main() {
+	//fmt.Println(utils.FindNeighbors("127.0.0.1", 5000, 0, 0, 5000, 5003))
+	fmt.Println(utils.GetHost())
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module blockchain
+
+go 1.21.5
+
+require (
+	github.com/btcsuite/btcutil v1.0.2
+	golang.org/x/crypto v0.18.0
+)
